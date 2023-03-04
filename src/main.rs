882d9a26fc02c8cd28a38c8c92a47c5601d55ff8
dxxxxy <introsdream@gmail.com>
2023-03-04 02:09:23 -0500

    feat: when set, remove and continue until no more

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,8 @@ fn main() {
     dotenv::dotenv().ok();
 
     let vanity_urls = get_vanity_urls();
-
     if vanity_urls.is_empty() {
-        println!("No vanity urls found");
+        log("No urls found!");
         exit(1);
     }
 
@@ -26,30 +25,30 @@ fn main() {
         let mut results = String::new();
 
         for (index, url) in vanity_urls.iter().enumerate() {
-
-            print!("\rChecking... ({}/{}) [round: {}] [mode: ∞]{}", index + 1, vanity_urls.len(), round, results);
-            stdout.flush().unwrap();
+            print!("Checking...");
 
             if check_vanity_url(&url) {
                 results.push_str(&format!(" {}", url.green()));
 
-                // if set_vanity_url(url) {
-                //     println!("{} setting Vanity URL to {}", "Succeeded".green(), url.green());
-                //     log(&format!("Succeeded setting Vanity URL to {}\n", url));
-                //     exit(0);
-                // } else {
-                //     println!("{} setting Vanity URL to {}", "Failed".red(), url.red());
-                //     log(&format!("Failed setting Vanity URL to {}\n", url));
-                // }
+                if set_vanity_url(url) {
+                    log(&format!("Succeeded setting Vanity URL to {}\n", url));
+                    get_vanity_urls().remove(index);
+
+                    if get_vanity_urls().is_empty() {
+                        exit(0);
+                    }
+                } else {
+                    log(&format!("Failed setting Vanity URL to {}\n", url));
+                }
             } else {
                 results.push_str(&format!(" {}", url.red()));
             }
+
+            print!("\rChecking... ({}/{}) [round: {}] [mode: ∞]{}", index + 1, vanity_urls.len(), round, results);
+            stdout.flush().unwrap();
         }
-
-        //last print
-        print!("\rChecking... ({}/{}) [round: {}] [mode: ∞]{}", vanity_urls.len(), vanity_urls.len(), round, results);
-
         round += 1;
+
 
         //wait 1 second then clear and start again
         println!("\nWaiting 1 second...");
